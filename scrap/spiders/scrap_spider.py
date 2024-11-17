7b24441df9b14d5a4e544cd2a302bd057d783538
MATUMOTO Eugene <yuu.mat.930@gmail.com>
2024-11-17 16:07:41 +0900

    update

---
+++ scrap/spiders/scrap_spider.py
@@ -0,0 +1,61 @@
+# import datetime
+import os
+from typing import Any, Optional
+from urllib.parse import urlparse
+
+import scrapy
+from scrapy.http import Response
+
+
+class ScrapSpider(scrapy.Spider):
+    name = "scrap"
+    allowed_domains = ["example.com"]
+    start_urls = ["http://www.example.com"]
+    max_depth = 2
+    visited: set[str] = set()
+
+    # custom_settings = {
+    #     "FEEDS": {
+    #         "dist/%(name)s_%(time)s.json": {
+    #             "format": "json",
+    #             "encoding": "utf8",
+    #             "store_empty": False,
+    #             "fields": None,
+    #             "indent": 4,
+    #             "overwrite": True,
+    #         }
+    #     },
+    #     "FEED_URI_PARAMS": lambda: {
+    #         "time": datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    #     }
+    # }
+
+    def __init__(self, name: Optional[str] = None, **kwargs: Any):
+        super().__init__(name, **kwargs)
+        os.makedirs("dist", exist_ok=True)
+
+        # Processing of command line argument receipt
+        if "url" in kwargs:
+            self.start_urls = [kwargs["url"]]
+            self.allowed_domains = [urlparse(kwargs["url"]).netloc]
+
+        if "depth" in kwargs:
+            self.max_depth = int(kwargs["depth"])
+
+    def parse(self, response: Response, crrent_depth=0):
+        self.visited.add(response.url)
+        if crrent_depth >= self.max_depth:
+            return
+
+        for anchor in response.xpath("//a"):  # type: ignore
+            text = anchor.xpath("normalize-space(text())").get()
+            link = response.urljoin(anchor.xpath("@href").get())
+
+            if link not in self.visited:
+                yield {"text": text, "link": link}
+
+                yield scrapy.Request(
+                    link,
+                    callback=self.parse,
+                    cb_kwargs={"crrent_depth": crrent_depth + 1},
+                )
