0aebf9b29fb0445a1836bb1d217d1cfeb8613bfc
MATUMOTO Eugene <yuu.mat.930@gmail.com>
2024-11-17 13:37:12 +0900

    restart project

--- scrap/items.py
+++ scrap/items.py
@@ -6,10 +6,7 @@
 import scrapy
 
 
-class Posts(scrapy.Item):
+class ScrapItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    url = scrapy.Field()
-    title = scrapy.Field()
-    date = scrapy.Field()
-    auther = scrapy.Field()
+    pass
